bfe9f33262a06e68fc0c15a079695add52e07cb8
imstefanny <imstefannyy@gmail.com>
2023-11-03 01:47:23 +0700

    fix bug

--- usecase/movie.go
+++ usecase/movie.go
@@ -168,7 +168,7 @@ func (s *movieUsecase) GetMovieRecommendations() ([]model.Movie, error) {
 
 	userInput := fmt.Sprintf("Here I get you JSON form of an array of movies data. %s Learn it and recommend me randomly choose three titles of them. Give me in the form ..., ..., ... WITHOUT other explanations.", string(moviesJSON))
 
-	godotenv.Load("../.env")
+	godotenv.Load(".env")
 	ctx := context.Background()
 	client := openai.NewClient(os.Getenv("KEY"))
 	messages := []openai.ChatCompletionMessage{
